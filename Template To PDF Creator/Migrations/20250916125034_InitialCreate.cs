4a42dbf47bc99222535282e4bef99081b726c1c9
Yuriy Balint <46222135+supalapka@users.noreply.github.com>
2025-09-16 15:54:41 +0300

    Added initial EF migration

---
+++ Template To PDF Creator/Migrations/20250916125034_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Template_To_PDF_Creator.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Templates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HtmlContent = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Templates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Templates");
+        }
+    }
+}
--- Template To PDF Creator/Model/Template.cs
+++ Template To PDF Creator/Model/Template.cs
@@ -1,7 +1,10 @@
-﻿namespace Template_To_PDF_Creator.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Template_To_PDF_Creator.Model
 {
     public class Template
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; } = "";
         public string HtmlContent { get; set; } = "";
